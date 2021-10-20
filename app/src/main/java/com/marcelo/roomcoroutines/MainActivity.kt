b9c80832751281954a14e2d03b1461660d93f386
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-10-20 14:40:50 -0300

    Toolbar Configured

--- app/src/main/java/com/marcelo/roomcoroutines/MainActivity.kt
+++ app/src/main/java/com/marcelo/roomcoroutines/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.marcelo.roomcoroutines
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.marcelo.roomcoroutines.ui.profile.ProfileFragment
-import com.marcelo.roomcoroutines.ui.start.StartFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(){
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupToolbar()
         setupFragments()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbarMain)
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     private fun setupFragments() {
