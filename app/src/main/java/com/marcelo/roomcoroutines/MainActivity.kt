f999324de12321f5f67f1f4e7d47c3c874bfc69d
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-10-20 00:10:58 -0300

    Navigation configured

--- app/src/main/java/com/marcelo/roomcoroutines/MainActivity.kt
+++ app/src/main/java/com/marcelo/roomcoroutines/MainActivity.kt
@@ -2,10 +2,18 @@ package com.marcelo.roomcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marcelo.roomcoroutines.ui.profile.ProfileFragment
+import com.marcelo.roomcoroutines.ui.start.StartFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupFragments()
+    }
+
+    private fun setupFragments() {
+
     }
 }
